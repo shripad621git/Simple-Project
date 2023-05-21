096204cedcf59a6c9d63ef475f314ba9843e4fff
shripad621git <shripad62001@gmail.com>
2023-05-21 12:23:53 +0530

    Modified calculator file

--- Calculator Project/Calculator.java
+++ Calculator Project/Calculator.java
@@ -5,5 +5,6 @@ class HelloWorld
    public static void main (String [] args)
    { 
       System.out.println("Hello I am Calculator");
+      System.out.println("Hello I am Shripad Calculator");
    }
 }
